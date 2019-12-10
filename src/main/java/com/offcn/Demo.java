9f25d0d9262a0021b961a5a8aa45fef064c99702
lyh-java <505229040@qq.com>
2019-12-10 20:54:21 +0800

    新增demo

---
+++ src/main/java/com/offcn/Demo.java
@@ -0,0 +1,20 @@
+package com.offcn;
+
+/**
+ * @Author lin
+ * @create 2019-12-10 20:27
+ */
+public class Demo {
+    public static  void main(String[] args){
+        get();
+        System.out.println("孙子文沙雕！");
+        System.out.println("孙子文傻点点！");
+        show();
+    }
+    public static  void get(){
+        System.out.println("我是孙子文！");
+    }
+    public static  void show(){
+        System.out.println("我来啦！");
+    }
+}
